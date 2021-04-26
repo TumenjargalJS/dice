3fecce95f749d5b523da163f8e43aba29f1df888
Tumee <tumenjargal.b14@gmail.com>
2021-04-26 21:45:17 +0800

    shoo soligddog bolson

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ var scores = [0, 0];
 // тоглогчийн ээлжийн оноо хадгалах хувьсагч
 var roundScore = 0;
 // шооны буусан талын хадгалах хувьсагч. 1-6 гэсэн тооны аль нэг буух ёстой.
-var dice = Math.floor(Math.random()*6) + 1;
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = 'none';
+
+
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function(){
+    var diceNumber = Math.floor(Math.random()*6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + '.png'; 
+    // alert("Шоог шидлээ : " + diceNumber);
+}
+);
